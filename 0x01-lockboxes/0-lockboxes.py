115f4fb7f61cfadeef2c05a427c0645c5361c2de
Gehadazzam <gehadgamalazzam@gmail.com>
2024-07-04 21:35:11 +0300

    update style

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,12 +3,14 @@
 
 
 def canUnlockAll(boxes):
+
     """boxes is a list of lists
     A key with the same number as a box opens that box
     You can assume all keys will be positive integers
     There can be keys that do not have boxes
     The first box boxes[0] is unlocked
     Return True if all boxes can be opened, else return False"""
+
     # Initialize a set to keep track of visited boxes
     boxesVisited = set()
 
@@ -18,7 +20,8 @@ def canUnlockAll(boxes):
 
         # Iterate over each key in the current box
         for key in boxes[box]:
-            # Check if the key corresponds to a valid box that hasn't been visited yet
+            # Check if the key corresponds to a valid box
+            # that hasn't been visited yet
             if 0 <= key < len(boxes) and key not in boxesVisited:
                 # Recursively try to open the box corresponding to the key
                 dfs(key)
@@ -26,5 +29,6 @@ def canUnlockAll(boxes):
     # Start DFS from the first box (which is always unlocked)
     dfs(0)
 
-    # If there are any unvisited boxes left, return False; otherwise, return True
+    # If there are any unvisited boxes left,
+    # return False; otherwise, return True
     return len(boxesVisited) == len(boxes)
